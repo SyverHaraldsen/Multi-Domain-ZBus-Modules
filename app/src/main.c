9255ec976125305e495837cbbd4f92138ea0f5d5
Syver Haraldsen <syver.haraldsen@nordicsemi.no>
2025-10-16 11:11:53 +0200

    modules: Rename BLE module to ble_nus and improve build system.
    Rename BLE module to ble_nus and improve build system with overlay
    config support.
    
    Signed-off-by: Syver Haraldsen <syver.haraldsen@nordicsemi.no>

--- app/src/main.c
+++ app/src/main.c
@@ -6,10 +6,12 @@
 
 #include <zephyr/kernel.h>
 #include <zephyr/zbus/zbus.h>
-#include <zephyr/zbus/multidomain/zbus_multidomain.h>
 
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
+
+#if IS_ENABLED(CONFIG_ZBUS_MULTIDOMAIN)
+#include <zephyr/zbus/multidomain/zbus_multidomain.h>
 
 /* Set up UART proxy agent, with no channels attached this will just receive
  * messages from the other domain and forward them to the local zbus.
@@ -17,10 +19,16 @@ LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
 #define ZBUS_UART_NODE DT_ALIAS(zbus_uart)
 ZBUS_PROXY_AGENT_DEFINE(uart_proxy, ZBUS_MULTIDOMAIN_TYPE_UART, ZBUS_UART_NODE);
 
-#if IS_ENABLED(CONFIG_MDM_BLE)
-ZBUS_CHAN_DECLARE(BLE_CHAN);
-ZBUS_PROXY_ADD_CHANNEL(uart_proxy, BLE_CHAN); /* Forward BLE_CHAN over uart*/
-#endif /* CONFIG_MDM_BLE */
+#if IS_ENABLED(CONFIG_MDM_BLE_NUS_RUNNER)
+ZBUS_CHAN_DECLARE(BLE_NUS_CHAN);
+ZBUS_PROXY_ADD_CHANNEL(uart_proxy, BLE_NUS_CHAN);
+#endif
+
+#if !IS_ENABLED(CONFIG_MDM_LED_RUNNER)
+ZBUS_CHAN_DECLARE(LED_CHAN);
+ZBUS_PROXY_ADD_CHANNEL(uart_proxy, LED_CHAN);
+#endif
+#endif
 
 int main(void)
 {
