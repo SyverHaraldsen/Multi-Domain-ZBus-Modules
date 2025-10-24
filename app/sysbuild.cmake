ea60311d662dc57be9cb8611d9aa851991851ef7
Syver Haraldsen <syver.haraldsen@nordicsemi.no>
2025-10-24 12:43:25 +0200

    modules: refactor config fragments and cmake lists to kconfig
    Replace config fragments and cmake lists with kconfig-based configuration.
    
    Signed-off-by: Syver Haraldsen <syver.haraldsen@nordicsemi.no>

--- app/sysbuild.cmake
+++
@@ -1,6 +0,0 @@
-# Copyright (c) 2025 Nordic Semiconductor ASA
-# SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
-
-if(CONFIG_MDM_CHANNEL_SOUNDING)
-	include(${CMAKE_CURRENT_LIST_DIR}/../modules/channel_sounding/sysbuild_fragment.cmake)
-endif()
